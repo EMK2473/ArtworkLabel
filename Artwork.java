cf78e302f5ebf4abc958f5a05b1853337e07f6aa
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-10-01 16:24:31 -0400

    removed instuctional comments;

--- Artwork.java
+++ Artwork.java
@@ -1,19 +1,13 @@
 public class Artwork {
-    // TODO: Declare private fields - title, yearCreated
     private String title;
     private int yearCreated;
-    
-    // TODO: Declare private field artist of type Artist 
     private Artist artist;    
     
-    // TODO: Define default constructor
     public Artwork() {
         this.title = "unknown";
         this.yearCreated = -1;
         this.artist = new Artist();
     }
-    
-    // TODO: Define get methods: getTitle(), getYearCreated()
 
     public String getTitle() {
         return title;
@@ -22,9 +16,6 @@ public class Artwork {
     public int getYearCreated(){
         return yearCreated;
     }
- 
-    // TODO: Define second constructor to initialize 
-    //       private fields (title, yearCreated, artist)
 
     public Artwork(String artTitle, int artYearCreated, Artist artist) {
         this.title = artTitle;
@@ -33,8 +24,6 @@ public class Artwork {
 
     }
  
-    // TODO: Define printInfo() method
-    //       Call the printInfo() method in Artist.java to print an artist's information
     public void printInfo(){
         artist.printInfo();
         System.out.println("Title: " + title + ", " + yearCreated);

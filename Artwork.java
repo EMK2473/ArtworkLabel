3c2a45731f91e355a0af74aee1ee8b9de9bd82c7
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-10-01 15:58:32 -0400

    defined getter methods: getTitle, getYearCreated; defined second constr to initialize artwork class;

--- Artwork.java
+++ Artwork.java
@@ -14,9 +14,24 @@ public class Artwork {
     }
     
     // TODO: Define get methods: getTitle(), getYearCreated()
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYearCreated(){
+        return yearCreated;
+    }
  
     // TODO: Define second constructor to initialize 
     //       private fields (title, yearCreated, artist)
+
+    public Artwork(String artTitle, int artYearCreated, Artist artist) {
+        this.title = artTitle;
+        this.yearCreated = artYearCreated;
+        this.artist = artist
+
+    }
  
     // TODO: Define printInfo() method
     //       Call the printInfo() method in Artist.java to print an artist's information                                                                                          

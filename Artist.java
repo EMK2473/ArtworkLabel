f7ef9f7a819674d193e2fc8d0310e318cd469e19
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-10-01 15:45:08 -0400

    defined get methods getName, getBirthYear, getDeathYear;

--- Artist.java
+++ Artist.java
@@ -14,12 +14,23 @@ public class Artist {
     // TODO: Define second constructor to initialize 
     //       private fields (artistName, birthYear, deathYear)
     public Artist(String name, int yearBorn, int yearDied) {
-      this.artistName = name;
-      this.birthYear = yearBorn;
-      this.deathYear = yearDied;
-   }
+        this.artistName = name;
+        this.birthYear = yearBorn;
+        this.deathYear = yearDied;
+    }
     
     // TODO: Define get methods: getName(), getBirthYear(), getDeathYear()
+    public String getName(){
+        return artistName;
+    }
+     
+     public int getBirthYear() {
+        return birthYear;   
+    }
+     
+     public int getDeathYear() {
+        return deathYear;
+    }
  
     // TODO: Define printInfo() method
     //       If deathYear is entered as -1, only print birthYear

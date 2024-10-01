f436a323e409c9f86912984cd88423d1e8bb279d
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-10-01 15:43:38 -0400

    for artist.java: declared private fields; defined default constructor;

--- Artist.java
+++ Artist.java
@@ -1,10 +1,19 @@
 public class Artist {
     // TODO: Declare private fields - artistName, birthYear, deathYear
- 
+    private String artistName;
+    private int birthYear;
+    private int deathYear;
+
     // TODO: Define default constructor
+    public Artist() {
+        this.artistName = "unknown";
+        this.birthYear = -1;
+        this.deathYear = -1;
+    }
  
     // TODO: Define second constructor to initialize 
     //       private fields (artistName, birthYear, deathYear)
+    
     
     // TODO: Define get methods: getName(), getBirthYear(), getDeathYear()
  
